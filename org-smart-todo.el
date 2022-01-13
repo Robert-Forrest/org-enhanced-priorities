13005d3aa822d7dfc89b9d1485503bb10a64e803
Robert Forrest <robertforrest@live.com>
2022-01-13 09:15:31 +0000

    Renamed variables to org-enhanced-priorities

--- org-smart-todo.el
+++ org-smart-todo.el
@@ -1,14 +1,14 @@
-;;; org-smart-todo.el --- Rank TODOs by priority, impact, and work  -*- lexical-binding: t; -*-
+;;; org-enhanced-priorities.el --- Rank TODOs by priority, impact, and work  -*- lexical-binding: t; -*-
 
 ;; Author: Robert Forrest <robertforrest@live.com>
-;; Url: https://github.com/Robert-Forrest/org-smart-todo
+;; Url: https://github.com/Robert-Forrest/org-enhanced-priorities
 ;; Version: 0.1-pre
 ;; Package-Requires:  ((emacs "26.1") (org "9.0"))
 ;; Keywords: Org, agenda, tasks
 
 ;;; Commentary:
 
-;;; Extends Org-mode's default priority-based ranking with impact and work
+;;; Enhances Org-mode's default priority-based ranking with impact and work
 
 ;;; License:
 
@@ -53,70 +53,70 @@
 
 ;;;; Customization
 
-(defgroup org-smart-todo nil
-  "Settings for `org-smart-todo'."
+(defgroup org-enhanced-priorities nil
+  "Settings for `org-enhanced-priorities'."
   :group 'org
-  :link '(url-link "http://github.com/robert-forrest/org-smart-todo"))
+  :link '(url-link "http://github.com/robert-forrest/org-enhanced-priorities"))
 
-(defcustom org-smart-todo-weight-priority 2.5
+(defcustom org-enhanced-priorities-weight-priority 2.5
   "Default value for weight of priority in ranking."
   :type 'number
-  :group 'org-smart-todo)
+  :group 'org-enhanced-priorities)
 
-(defcustom org-smart-todo-weight-impact 2
+(defcustom org-enhanced-priorities-weight-impact 2
   "Default value for weight of impact in ranking."
   :type 'number
-  :group 'org-smart-todo)
+  :group 'org-enhanced-priorities)
 
-(defcustom org-smart-todo-weight-work -1
+(defcustom org-enhanced-priorities-weight-work -1
   "Default value for weight of work in ranking."
   :type 'number
-  :group 'org-smart-todo)
+  :group 'org-enhanced-priorities)
 
 ;;;; Functions
 
-(defun org-smart-todo--get-property (property &optional pos)
+(defun org-enhanced-priorities--get-property (property &optional pos)
   "Get PROPERTY from org item at POS or under cursor."
   (org-entry-get (or pos (point)) property))
 
-(defun org-smart-todo--get-numerical-property (property &optional pos)
+(defun org-enhanced-priorities--get-numerical-property (property &optional pos)
   "Convert string PROPERTY values for item at POS or under cursor to numbers."
-  (let*((property-value (or (org-smart-todo--get-property property pos) "0")))
+  (let*((property-value (or (org-enhanced-priorities--get-property property pos) "0")))
     (if (string= property "PRIORITY")
         (cond ((string= property-value "A") 3)
               ((string= property-value "B") 2)
               ((string= property-value "C") 1))
       (string-to-number property-value))))
 
-(defun org-smart-todo--get-marker (item)
+(defun org-enhanced-priorities--get-marker (item)
   "Get the org-marker from text properties of ITEM."
   (or (get-text-property 0 'org-marker item)
       (get-text-property 0 'org-hd-marker item)))
 
-(defun org-smart-todo--calculate-overall-priority (&optional pos)
+(defun org-enhanced-priorities--calculate-overall-priority (&optional pos)
   "Calculate the overall priority of an Org item either at POS or under cursor."
-  (let*((impact (org-smart-todo--get-numerical-property "impact" pos))
-        (work (org-smart-todo--get-numerical-property "work" pos))
-        (priority (org-smart-todo--get-numerical-property "PRIORITY" pos))
+  (let*((impact (org-enhanced-priorities--get-numerical-property "impact" pos))
+        (work (org-enhanced-priorities--get-numerical-property "work" pos))
+        (priority (org-enhanced-priorities--get-numerical-property "PRIORITY" pos))
         (total (+
-                (* org-smart-todo-weight-priority priority)
-                (* org-smart-todo-weight-impact impact)
-                (* org-smart-todo-weight-work work))))
+                (* org-enhanced-priorities-weight-priority priority)
+                (* org-enhanced-priorities-weight-impact impact)
+                (* org-enhanced-priorities-weight-work work))))
     total))
   
-(defun org-smart-todo--agenda-sort-overall-priority (a b)
+(defun org-enhanced-priorities--agenda-sort-overall-priority (a b)
   "Compare the overall priority values of items A and B."
-  (let*((ma (org-smart-todo--get-marker a))
-        (mb (org-smart-todo--get-marker b))
+  (let*((ma (org-enhanced-priorities--get-marker a))
+        (mb (org-enhanced-priorities--get-marker b))
         
-        (total-a (org-smart-todo--calculate-overall-priority ma))
-        (total-b (org-smart-todo--calculate-overall-priority mb)))
+        (total-a (org-enhanced-priorities--calculate-overall-priority ma))
+        (total-b (org-enhanced-priorities--calculate-overall-priority mb)))
     
     (cond ((> total-a total-b) +1)
           ((< total-a total-b) -1))))
 
 ;;;; Footer
 
-(provide 'org-smart-todo)
+(provide 'org-enhanced-priorities)
 
-;;; org-smart-todo.el ends here
+;;; org-enhanced-priorities.el ends here
