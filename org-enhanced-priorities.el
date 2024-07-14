bc2bfc25d43242353a388411e90a5826ebe62578
Robert Forrest <robertforrest@live.com>
2024-07-14 14:47:44 +0100

    feat: deadlines and schedules now properly impact priorities

--- org-enhanced-priorities.el
+++ org-enhanced-priorities.el
@@ -57,7 +57,12 @@
   :group 'org
   :link '(url-link "http://github.com/robert-forrest/org-enhanced-priorities"))
 
-(defcustom org-enhanced-priorities-property-weights '(("PRIORITY" . 7.5) ("DEADLINE" . 2) ("impact" . 2) ("difficulty" . -1))
+(defcustom org-enhanced-priorities-property-weights
+  '(("PRIORITY" . 7.5)
+    ("DEADLINE" . 4)
+    ("SCHEDULED" . 4)
+    ("impact" . 2)
+    ("difficulty" . -1))
   "Association list of property names and weights to be considered while ranking."
   :type 'alist
   :group 'org-enhanced-priorities)
@@ -85,11 +90,12 @@
   (let*((property-value (or (org-enhanced-priorities--get-property property pos) "0")))
     (cond ((string= property "PRIORITY") (org-enhanced-priorities--get-numerical-priority pos))
           ((string= property "DEADLINE") (org-enhanced-priorities--get-deadline-contribution pos))
+          ((string= property "SCHEDULED") (org-enhanced-priorities--get-scheduled-contribution pos))
           (t (string-to-number property-value)))))
 
 ;;;###autoload
 (defun org-enhanced-priorities--get-numerical-priority (&optional pos)
-  "Map current Org entry priority to its index between `org-lowest-priority` and `org-highest-priority`."
+  "Map Org entry (at POS or under cursor) priority to its index between `org-lowest-priority` and `org-highest-priority`."
   (let ((priority (org-entry-get (or pos (point)) "PRIORITY")))
     (if priority
         (/ (float (- org-lowest-priority (string-to-char priority))) (- org-lowest-priority org-highest-priority))
@@ -97,11 +103,15 @@
 
 ;;;###autoload
 (defun org-enhanced-priorities--get-deadline-contribution (&optional pos)
-  "Send DEADLINE property through gradient function to get priority contribution, return zero otherwise."
+  "Send DEADLINE property for entry at POS or under cursor through gradient function to get priority contribution, return zero otherwise."
   (let ((deadline (org-enhanced-priorities--get-property "DEADLINE" pos)))
-    (if deadline
-        (max 0 (+ (* org-enhanced-priorities-deadline-gradient (org-time-stamp-to-now deadline)) 3))
-      0)))
+    (if deadline (+ (* org-enhanced-priorities-deadline-gradient (org-time-stamp-to-now deadline)) 3) 0)))
+
+;;;###autoload
+(defun org-enhanced-priorities--get-scheduled-contribution (&optional pos)
+  "Ramp a priority debuff as scheduled date for task at POS or under cursor approaches."
+  (let ((scheduled (org-enhanced-priorities--get-property "SCHEDULED" pos)))
+    (if scheduled (* org-enhanced-priorities-deadline-gradient (org-time-stamp-to-now scheduled)) 0)))
 
 ;;;###autoload
 (defun org-enhanced-priorities--get-marker (item)
@@ -118,8 +128,7 @@
 ;;;###autoload
 (defun org-enhanced-priorities--calculate-overall-priority (&optional pos)
   "Calculate the overall priority of an Org item either at POS or under cursor."
-  (cl-loop for (property . value) in (org-enhanced-priorities--calculate-priority-components pos)
-           sum value))
+  (cl-loop for (property . value) in (org-enhanced-priorities--calculate-priority-components pos) sum value))
 
 ;;;###autoload
 (defun org-enhanced-priorities--agenda-sort-overall-priority (a b)
@@ -132,10 +141,11 @@
           ((< total-a total-b) -1))))
 
 ;;;###autoload
-(defun org-enhanced-priorities--highest-priority (&optional match)
+(defun org-enhanced-priorities--highest-priority (&optional match scope)
   "Open the Org entry with the highest priority.
-MATCH specifies the entries to match, defaulting to \"/TODO\"."
-  (interactive (list (read-string "Match: " "/TODO")))
+MATCH specifies the entries to match, defaulting to \"/TODO\".
+SCOPE specifies where org entries will be sourced from, defaulting to 'agenda"
+  (interactive (list (read-string "Match: " "/TODO") (intern (read-string "Scope: " "agenda"))))
   (let ((max-priority -999)
         max-heading max-marker)
     (org-map-entries
@@ -145,12 +155,57 @@ MATCH specifies the entries to match, defaulting to \"/TODO\"."
            (setq max-priority priority
                  max-heading (org-get-heading t t t t)
                  max-marker (point-marker)))))
-     (or match "/TODO") 'agenda)
+     (or match "/TODO") (or scope 'agenda))
     (when max-marker
       (switch-to-buffer (marker-buffer max-marker))
       (goto-char max-marker)
       (org-fold-show-entry)
       (message "\"%s\" (priority: %d)" max-heading max-priority))))
+
+;;;###autoload
+(defun org-enhanced-priorities--sorted-entries (&optional match scope)
+  "Display Org entries sorted by their priority.
+MATCH specifies the entries to match, defaulting to \"/TODO\".
+SCOPE specifies where org entries will be sourced from, defaulting to 'agenda."
+  (interactive (list (read-string "Match: " "/TODO") (intern (read-string "Scope: " "agenda"))))
+  (let (entries)
+    (org-map-entries
+     (lambda ()
+       (let* ((todo-state (org-no-properties (org-get-todo-state)))
+              (priority (org-enhanced-priorities--calculate-overall-priority))
+              (heading (org-get-heading t t t t))
+              (entry-text (concat "* " todo-state " [#" (number-to-string (round priority)) "] " heading))
+              (marker (point-marker)))
+         (when todo-state
+           (push (list entry-text marker priority) entries))))
+     (or match "/TODO") scope)
+    ;; Sort entries by priority (first element in each sublist)
+    (setq entries (reverse (sort entries (lambda (a b) (< (caddr a) (caddr b))))))
+    (with-temp-buffer-window "*Org Entries Sorted by Priority*" nil nil
+      (with-current-buffer "*Org Entries Sorted by Priority*"
+        (switch-to-buffer-other-window "*Org Entries Sorted by Priority*")
+        (let ((inhibit-read-only t))
+          (org-mode)
+          (dolist (entry entries)
+            (let ((entry-text (car entry))
+                  (marker (cadr entry)))
+              (goto-char (point-max))
+              (put-text-property 0 (length entry-text) 'org-marker marker entry-text)
+              (insert (concat entry-text "\n")))))
+        (setq buffer-read-only t)
+        ;; Define a local keymap with bindings for 'q' and 'C-g' to quit the buffer
+        (let ((map (make-sparse-keymap)))
+          (define-key map (kbd "q") 'quit-window)
+          (define-key map (kbd "C-g") 'quit-window)
+          (define-key map (kbd "RET")
+                      (lambda ()
+                        (interactive)
+                        (let ((marker (get-text-property (point) 'org-marker)))
+                          (when marker
+                            (switch-to-buffer (marker-buffer marker))
+                            (goto-char marker)
+                            (org-fold-show-subtree)))))
+          (use-local-map map))))))
 ;;;; Footer
 
 (provide 'org-enhanced-priorities)
